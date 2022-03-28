83bfc73145ff2bf06335ba919565759c595224c5
Nora <noraonyi@gmail.com>
2022-03-28 07:54:25 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(distance){
+const headQuarter = 42;
+
+if (headQuarter > distance){
+     return headQuarter -distance
+} else{ return distance -headQuarter}
+
+  return Math.abs(
+distance - headQuarter);
+// console.log(distance);
+//return   
+}
